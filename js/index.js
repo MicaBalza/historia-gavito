da3649623c2c604c8ae11c9fcb2d8c48be4fc151
MicaBalza <mica.balza@gmail.com>
2020-09-23 03:09:54 -0300

    WIP nav/DOM/events

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
+let root = document.documentElement;
+let themeSelect = document.querySelector("#theme-select");
+
+themeSelect.addEventListener("change", changeTheme);
+
+function changeTheme(e) {
+  if (e.target.value === "dark") {
+    root.style.setProperty("--color-background", "#0B132B");
+    root.style.setProperty("--color-surface", "#1C2541");
+    root.style.setProperty("--color-text", "#5BC0BE");
+    root.style.setProperty("--color-accent", "#6FFFE9");
+  } else if (e.target.value === "light") {
+    root.style.setProperty("--color-background", "");
+    root.style.setProperty("--color-surface", "");
+    root.style.setProperty("--color-text", "");
+    root.style.setProperty("--color-accent", "");
+  }
+}
+
 var period = [
   "Modernidad",
   "Posmodernidad",
