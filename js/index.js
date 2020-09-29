06410fcdf3770fc6653358181df25ccf4967b729
MicaBalza <mica.balza@gmail.com>
2020-09-29 20:39:08 -0300

    Separated js themeSelect

--- js/index.js
+++ js/index.js
@@ -1,23 +1,6 @@
-// ----- CHANGE THEME
+// ----- SPINNER
 
-let root = document.documentElement;
-let themeSelect = document.querySelector("#theme-select");
-
-themeSelect.addEventListener("change", changeTheme);
-
-function changeTheme(e) {
-  if (e.target.value === "dark") {
-    root.style.setProperty("--color-background", "#0B132B");
-    root.style.setProperty("--color-surface", "#1C2541");
-    root.style.setProperty("--color-text", "#5BC0BE");
-    root.style.setProperty("--color-accent", "#6FFFE9");
-  } else if (e.target.value === "light") {
-    root.style.setProperty("--color-background", "");
-    root.style.setProperty("--color-surface", "");
-    root.style.setProperty("--color-text", "");
-    root.style.setProperty("--color-accent", "");
-  }
-}
+document.addEventListener("DOMContentLoaded");
 
 // ----- GET USER NAME
 function getName() {
